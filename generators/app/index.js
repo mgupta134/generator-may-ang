cd0a3cd2155df0c1e1b877f194692d9c7b351ed7
Mayank Gupta <mgupta@Mayanks-MacBook-Pro.local>
2016-03-17 17:37:39 -0700

    adding application files and index.html file. Also, updated bower template creating and passing use selected modules.

--- generators/app/index.js
+++ generators/app/index.js
@@ -28,11 +28,11 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     /*
-    *   Prompt user application based questions
-    *   1) App Name?
-    *   2) Author Name?
-    *   3) Include default module (like landing page or something)?  
-    */
+     *   Prompt user application based questions
+     *   1) App Name?
+     *   2) Author Name?
+     *   3) Include default module (like landing page or something)?  
+     */
     promptApplicationInfo: function() {
         var done = this.async();
 
@@ -52,7 +52,7 @@ module.exports = yeoman.generators.Base.extend({
         this.prompt(prompts, function(props) {
             //slugish app name => if user enters "Hello World" it will be "hello-world"
             props.slugishAppName = _.slugify(props.appName)
-            //camelize app name => result for "Hello World" will be "helloWorld"
+                //camelize app name => result for "Hello World" will be "helloWorld"
             props.camelizeAppName = _.camelize(_.decapitalize(props.appName));
             this.rootAppFolder = props.camelizeAppName;
             // To access props later use this.props.someOption;
@@ -63,11 +63,11 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     /*  Prompt user to include most common angular modules
-    *   1) Angular Cookies
-    *   2) Angular Animate
-    *   3) Angular Touch 
-    *   4) Angulat Sanitize
-    */
+     *   1) Angular Cookies
+     *   2) Angular Animate
+     *   3) Angular Touch 
+     *   4) Angulat Sanitize
+     */
     promptModulesDependecies: function() {
         var done = this.async();
 
@@ -133,15 +133,22 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     copyRootFiles: function() {
+        var props = this.props;
         //Package file
         this.fs.copy(
             this.templatePath('root/_package.json'),
             this.destinationPath(this.rootAppFolder + '/package.json')
         );
         //Bower and bowerrrc
-        this.fs.copy(
+        this.fs.copyTpl(
             this.templatePath('root/_bower.json'),
-            this.destinationPath(this.rootAppFolder + '/bower.json')
+            this.destinationPath(this.rootAppFolder + '/bower.json'), {
+                slugishAppName: props.slugishAppName,
+                ngCookies: _.contains(props.angularModules, 'angularCookies'),
+                ngAnimate: _.contains(props.angularModules, 'angularAnimate'),
+                ngTouch: _.contains(props.angularModules, 'angularTouch'),
+                ngSanitize: _.contains(props.angularModules, 'angularSanitize')
+            }
         );
         this.fs.copy(
             this.templatePath('root/_bowerrc'),
@@ -169,38 +176,31 @@ module.exports = yeoman.generators.Base.extend({
         );
     },
 
-    copyAppFiles: function(){
-       /* var props = this.props;
+    copyAppFiles: function() {
+        var props = this.props;
 
+        //Copy Index File
         this.fs.copyTpl(
             this.templatePath('app/_index.html'),
-            this.destinationPath('app/index.html'), {
+            this.destinationPath(this.rootAppFolder + '/app/index.html'), {
                 camelizeAppName: props.camelizeAppName
             }
         );
 
+        //Copy application file
         this.fs.copy(
             this.templatePath('app/scripts/_application.js'),
-            this.destinationPath('app/scripts/application.js')
+            this.destinationPath(this.rootAppFolder +'/app/scripts/application.js')
         );
+
+        //Copy config file
         this.fs.copyTpl(
             this.templatePath('app/scripts/_config.js'),
-            this.destinationPath('app/scripts/config.js'), {
+            this.destinationPath(this.rootAppFolder +'/app/scripts/config.js'), {
                 camelizeAppName: props.camelizeAppName,
                 appDependencies: props.angularModules
             }
         );
-
-        this.fs.copyTpl(
-            this.templatePath('_bower.json'),
-            this.destinationPath('bower.json'), {
-                slugishAppName: props.slugishAppName,
-                ngCookies: _.contains(props.angularModules, 'angularCookies'),
-                ngAnimate: _.contains(props.angularModules, 'angularAnimate'),
-                ngTouch: _.contains(props.angularModules, 'angularTouch'),
-                ngSanitize: _.contains(props.angularModules, 'angularSanitize')
-            }
-        );*/
 
     },
 
